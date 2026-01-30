edbac855a2a76b6728bf1063b3d88fb14bd7f5f9
Clifford William <ccliffordwilliam@gmail.com>
2026-01-30 20:27:58 +0700

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "@fastify/cors";
 import type { FastifyPluginAsync } from "fastify";
 import db from "./config/db.js";
 import errorHandler from "./config/error-handler.js";
@@ -5,6 +6,7 @@ import itemRoutes from "./domains/items/item-routes.js";
 import orderRoutes from "./domains/orders/order-routes.js";
 
 const app: FastifyPluginAsync = async (fastify) => {
+	await fastify.register(cors, { origin: "*" });
 	errorHandler(fastify);
 	db(fastify);
 	fastify.register(orderRoutes, { prefix: "orders" });
